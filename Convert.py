3dfdeca9879f7d3b5131c0c6cc727557b75563df
FAREAST\yueych <daniyuu@163.com>
2018-07-12 13:15:31 +0800

    fix some bug and remove log print message

--- Convert.py
+++ Convert.py
@@ -1,6 +1,7 @@
-input_file = open("./data/test.txt", "r", encoding="utf-8")
+file_name = "validation.txt"
 
-output_file = open("./result/test.txt", "w", encoding="utf-8")
+input_file = open("./data/{0}".format(file_name), "r", encoding="utf-8")
+output_file = open("./result/{0}".format(file_name), "w", encoding="utf-8")
 
 
 def reformatWordMark(word):
@@ -12,23 +13,25 @@ def reformatWordMark(word):
             word[-1] = word[-1][0:2] + "E" + word[-1][3:]
     return word
 
+
 word = []
 text = ""
 entity = ""
+index = 0
 for line in input_file.readlines():
+    print(index)
+    index += 1
     if (len(line) > 2):
         text = line[0]
         mark = line[2]
 
-        if mark == "O" or mark == "B" and len(word) != 0:
-            word = reformatWordMark(word)
-            for newLine in word:
-                output_file.write(newLine)
-                print(newLine)
-            word = []
+        if mark == "O" or mark == "B":
+            if len(word) != 0:
+                word = reformatWordMark(word)
+                for newLine in word:
+                    output_file.write(newLine)
+                word = []
         word.append(line)
 
     else:
-        # output_file.write(line)
-        # print(line)
         word.append(line)
